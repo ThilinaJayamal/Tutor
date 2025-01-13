7245bcf61c3f04af38ee83b1bc7636c527422154
ThilinaJayamal <thilinajayamal271@gmail.com>
2025-01-13 14:51:55 +0530

    complete landing page version-1

---
+++ script.js
@@ -0,0 +1,20 @@
+AOS.init();
+
+const videoContainer = document.getElementById("vcontainer");
+const player = document.getElementById("player");
+const playBtn = document.getElementById("play");
+
+document.addEventListener("click",(e)=>{
+    e.stopPropagation();
+})
+
+document.addEventListener("click",(e)=>{
+    videoContainer.classList.add("hidden");
+})
+
+playBtn.addEventListener("click",(e)=>{
+    e.stopPropagation();
+    videoContainer.classList.remove("hidden");
+})
+
+
